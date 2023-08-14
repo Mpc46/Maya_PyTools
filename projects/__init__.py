b0ce0caf15e8bc6110479eae92b6ddf35618705f
Luis F. Carranza <LuisF.Carranza@Outlook.com>
2023-08-14 15:51:43 -0600

    XD

---
+++ projects/__init__.py
@@ -0,0 +1,17 @@
+'''
+/*****************************************************************************/
+                        Rigging Tools and Proyects
+                     ________________________________________
+                    |                                        |
+                    |  Author: Luis Felipe Carranza          |
+                    |________________________________________|
+
+>> DESCRIPTION >>
+    My python tools and projects for rigging in Autodesk Maya
+
+>> CONTENTS >> 
+    + modules
+    + projects
+
+/*****************************************************************************
+'''
