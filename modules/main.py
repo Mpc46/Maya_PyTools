c0ddf055f9bace0e667e47c3ab8c49d665a290a1
Luis F. Carranza <LuisF.Carranza@Outlook.com>
2023-08-15 16:53:53 -0600

    Main

--- modules/main.py
+++ modules/main.py
@@ -12,13 +12,6 @@
 >> DEPENDENCIES >>
     - Maya cmds and OpenMaya (Requieres Autodesk Maya, pre-installed)
     - six (python.exe -m pip install six) [install trough pip]
-
->> NOTES >> 
-	Update 02/08/2023 : Start working on the script.
-
->> THANKS >> 
-    Nick Hughes [02/08/2023]:
-        For his awesome course that led me to create this file. 
  
 >> CONTACT >>
     luisf.carranza@outlook.com
